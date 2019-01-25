61a9d13c77738644920cb469ac2ca50c6447fe8a
Nash Brancic <nashfft@gmail.com>
2019-01-25 03:48:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 def speak_to_grandma(respond)
   if respond.upcase != respond
     return "HUH?! SPEAK UP, SONNY!"
-  elsif 
+  elsif respond == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TO PUMPKIN!"
+  
     
